253b12d2998fab717d38c88c527596ae9c0c3c22
Stuart <stuart.chinery@gmail.com>
2019-08-20 10:47:58 +0100

    Aligned test setup with other Boxt gems

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,12 +6,22 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../test/dummy/config/environment.rb", __dir__)
 # ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 require "rails/test_help"
+
 require "minitest/autorun"
+require "minitest/bang"
+require "minitest/fail_fast"
+require "minitest/macos_notification"
+require "minitest/reporters"
 require "with_model"
 
-# Filter out Minitest backtrace while allowing backtrace from other libraries
-# to be shown.
-Minitest.backtrace_filter = Minitest::BacktraceFilter.new
+Minitest::Reporters.use!(
+  [
+    Minitest::Reporters::SpecReporter.new,
+    Minitest::Reporters::MacosNotificationReporter.new(title: "Filta gem")
+  ],
+  "test",
+  Minitest.backtrace_filter
+)
 
 # Rails::TestUnitReporter.executable = 'bin/test'
 
